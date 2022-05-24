38d5656c8c116b6e3630aa4adaf782ff58578281
DeveloperSenior <andresfescobar92@gmail.com>
2022-05-24 14:57:46 -0500

    kwargs["format"] = "%Y-%m-%d"

--- LogHours/LogHours/settings.py
+++ LogHours/LogHours/settings.py
@@ -119,7 +119,7 @@ TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
@@ -137,3 +137,11 @@ SWEETIFY_SWEETALERT_LIBRARY = 'sweetalert2'
 
 FILE_UPLOAD_HANDLERS = ("django_excel.ExcelMemoryFileUploadHandler",
                         "django_excel.TemporaryExcelFileUploadHandler")
+
+DATE_INPUT_FORMATS = [
+    '%Y-%m-%d', '%m/%d/%Y', '%m/%d/%y',  # '2006-10-25', '10/25/2006', '10/25/06'
+    '%b %d %Y', '%b %d, %Y',  # 'Oct 25 2006', 'Oct 25, 2006'
+    '%d %b %Y', '%d %b, %Y',  # '25 Oct 2006', '25 Oct, 2006'
+    '%B %d %Y', '%B %d, %Y',  # 'October 25 2006', 'October 25, 2006'
+    '%d %B %Y', '%d %B, %Y',  # '25 October 2006', '25 October, 2006'
+]
--- LogHours/LogHoursApp/forms.py
+++ LogHours/LogHoursApp/forms.py
@@ -5,13 +5,21 @@ from django.contrib.admin.widgets import AdminDateWidget
 from django.core.validators import FileExtensionValidator
 
 
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+    def __init__(self, **kwargs):
+        kwargs["format"] = "%Y-%m-%d"
+        super().__init__(**kwargs)
+
+
 class UploadFileForm(forms.Form):
     file = forms.FileField(required=True, label="Archivo",
                            validators=[FileExtensionValidator(allowed_extensions=["xls", "xlsx"])])
 
 
 class LogHoursForm(forms.ModelForm):
-    date = forms.DateTimeField(required=True, widget=AdminDateWidget)
+    date = forms.DateTimeField(required=True, widget=DateInput(attrs={'class': 'vTextField'}))
 
     class Meta:
         model = LogHours
